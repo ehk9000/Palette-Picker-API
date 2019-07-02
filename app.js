2dfdacac28b8814cc848d85f6f7aa1cf5f285ece
RyanDBarnett <conner2468@gmail.com>
2019-07-01 22:57:32 -0600

    Create projects get and post requests

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 const express = require('express');
 const cors = require('cors');
 
@@ -6,9 +10,25 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api/v1/projects', (req, res) => {
-  return res.status(200).json('test');
+  database('projects').select()
+  .then(projects => {
+    res.status(200).json(projects);
+  })
+  .catch((error) => {
+    res.status(500).json({ error });
+  });
 });
 
-// app.post('/api/v1/projects', (req, res) => {});
+app.post('/api/v1/projects', (req, res) => {
+  const {name} = req.body
+  if(!name) return res.status(422).json('Please name your project');
+  database('projects').insert({name}, 'id')
+  .then(id => {
+    res.status(201).json({name, id: id[0]});
+  })
+  .catch(error => {
+    res.status(500).json({ error });
+  })
+});
 
 module.exports = app;
