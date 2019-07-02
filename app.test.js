fe35fe1d680d94a5a1f3e7dfe9ef128f8fe838a3
Erik Konner <erikkonner@gmail.com>
2019-07-02 15:40:16 -0600

    Test for projects/:id
    Co-authored-by: Justin Pyktel <justinpyktel@gmail.com>
    Co-authored-by: Ryan Barnett <conner2468@gmail.com>

--- app.test.js
+++ app.test.js
@@ -6,16 +6,34 @@ const database = require('knex')(configuration)
 
 
 describe('Server', () => {
+  beforeEach(async () => {
+    await database.seed.run
+  })
   describe('GET /projects', () => {
     it('should return all the projects in the DB ', async () => {
       // set up 
       const expectedProjects = await database('projects').select()
+
       //  execution
       const response = await request(app).get('/api/v1/projects')
       const projects = response.body
   
       // expectation
       expect(projects).toEqual(expectedProjects)
+    })
+  })
+  describe('GET /projects/:id', () => {
+    it('should return a single project with the id in the params', async () => {
+      const expectedProject = await database('projects').first()
+      console.log('expectedProject', expectedProject);
+      
+      const id = expectedProject.id;
+
+      const response = await request(app).get(`/projects/${id}`)
+      const project = response.body;
+      
+
+      expect(project).toEqual(expectedProject)
     })
   })
 })
