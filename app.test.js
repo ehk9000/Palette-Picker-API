a71878ab09e91dfa9dd5dc61ee8ed3ff161bbfd2
Erik Konner <erikkonner@gmail.com>
2019-07-02 15:57:18 -0600

    fixed timestamp datatype error. all tests so far passing
    Co-authored-by: Justin Pyktel <justinpyktel@gmail.com>
    Co-authored-by: Ryan Barnett <conner2468@gmail.com>

--- app.test.js
+++ app.test.js
@@ -13,6 +13,10 @@ describe('Server', () => {
     it('should return all the projects in the DB ', async () => {
       // set up 
       const expectedProjects = await database('projects').select()
+      expectedProjects.forEach(project => {
+        project.created_at = project.created_at.toJSON()
+        project.updated_at = project.updated_at.toJSON()
+      })
 
       //  execution
       const response = await request(app).get('/api/v1/projects')
@@ -25,15 +29,28 @@ describe('Server', () => {
   describe('GET /projects/:id', () => {
     it('should return a single project with the id in the params', async () => {
       const expectedProject = await database('projects').first()
-      console.log('expectedProject', expectedProject);
+      expectedProject.created_at = expectedProject.created_at.toJSON()
+      expectedProject.updated_at = expectedProject.updated_at.toJSON()
       
       const id = expectedProject.id;
 
-      const response = await request(app).get(`/projects/${id}`)
+      const response = await request(app).get(`/api/v1/projects/${id}`)
       const project = response.body;
       
 
       expect(project).toEqual(expectedProject)
     })
+  })
+  describe('POST /projects', () => {
+    it('should post a new project to the database', async () => {
+      const newProject = {name: 'BYOB'}
+
+      const response = await request(app).post('/api/v1/projects').send(newProject)
+      const id = response.body.id;
+      const project = await database('projects').where('id', id).first();
+
+      expect(newProject.name).toEqual(project.name)
+    })
+
   })
 })
