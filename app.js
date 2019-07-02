f8685f24530156c89cdff976e476b86b5160d3f3
Erik Konner <erikkonner@gmail.com>
2019-07-02 14:41:54 -0600

    fixed post projects/:id sad and happy paths
    Co-authored-by: Justin Pyktel <justinpyktel@gmail.com>
    Co-authored-by: Ryan Barnett <conner2468@gmail.com>

--- app.js
+++ app.js
@@ -20,8 +20,10 @@ app.get('/api/v1/projects', async (req, res) => {
 });
 
 app.get('/api/v1/projects/:id', async (req, res) => {
+  const id = req.params.id;
   try {
-    const project = database('projects').where('id', req.params.id).first()
+    const project = await database('projects').where('id', id).first()
+    if (!project) return res.status(404).json(`Cant find project with id ${id}`)
     res.status(200).json(project)
   }
   catch (error) {
@@ -29,16 +31,16 @@ app.get('/api/v1/projects/:id', async (req, res) => {
   }
 })
 
-app.post('/api/v1/projects', (req, res) => {
-  const {name} = req.body
-  if(!name) return res.status(422).json('Please name your project');
-  database('projects').insert({name}, 'id')
-  .then(id => {
+app.post('/api/v1/projects', async (req, res) => {
+  const { name } = req.body
+  try {
+    if(!name) return res.status(422).json('Please name your project');
+    const id = await database('projects').insert({name}, 'id')
     res.status(201).json({name, id: id[0]});
-  })
-  .catch(error => {
+  }
+  catch(error) {
     res.status(500).json({ error });
-  })
+  }
 });
 
 module.exports = app;
