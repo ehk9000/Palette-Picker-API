128404c35f31d5d11235f6d67448aa5cda3d61dc
SiimonStark <justinpyktel@gmail.com>
2019-07-03 22:55:12 -0600

    Tests written for Palette: Get & GetById, the endpoints had to be modified to get them functional. ALERT: Palette Post is incompletegit add .

--- app.test.js
+++ app.test.js
@@ -10,7 +10,7 @@ describe('Server', () => {
     await database.seed.run
   })
   describe('GET Methods for Palettes & Projects', () => {
-    describe('GET /projects', () => {
+    describe('GET /projects && /palettes', () => {
       it('should return all the projects in the DB ', async () => {
         // set up 
         const expectedProjects = await database('projects').select()
@@ -26,8 +26,19 @@ describe('Server', () => {
         // expectation
         expect(projects).toEqual(expectedProjects)
       })
+      it('should return all the palettes in the DB', async () => {
+        const expectedPalettes = await database('palettes').select()
+        expectedPalettes.forEach(palette => {
+          palette.created_at = palette.created_at.toJSON();
+          palette.updated_at = palette.updated_at.toJSON();
+        })
+
+        const response = await request(app).get('/api/v1/palettes')
+        const palettes = response.body;
+        expect(palettes).toEqual(expectedPalettes);
+      })
     })
-    describe('GET /projects/:id', () => {
+    describe('GET /projects/:id && /palettes/:id', () => {
       it('should return a single project with the id in the params', async () => {
         const expectedProject = await database('projects').first()
         expectedProject.created_at = expectedProject.created_at.toJSON()
@@ -41,10 +52,22 @@ describe('Server', () => {
   
         expect(project).toEqual(expectedProject)
       })
+      it('should return a single palette with the id in the params', async () => {
+        const expectedPalette = await database('palettes').first();
+        expectedPalette.created_at = expectedPalette.created_at.toJSON();
+        expectedPalette.updated_at = expectedPalette.updated_at.toJSON();
+
+        const id = expectedPalette.id;
+
+        const response = await request(app).get(`/api/v1/palettes/${id}`)
+        const palette = response.body;
+
+        expect(palette).toEqual(expectedPalette)
+      })
     })
   })
   describe('POST Methods for Palettes & Projects', () => {
-    describe('POST /projects', () => {
+    describe('POST /projects && /palettes', () => {
       it('should post a new project to the database', async () => {
         const newProject = {name: 'BYOB'}
 
@@ -54,9 +77,31 @@ describe('Server', () => {
 
         expect(newProject.name).toEqual(project.name)
       })
+      it('should post a new palette to the database', async () => {
+        let newPalette = {
+          palette_name: 'Test Put',
+          color_1: '3e3e3e',
+          color_2: '6f6f6f',
+          color_3: '7e7e7e',
+          color_4: 'eeeeee',
+          color_5: '999999'
+        }
+
+        const response = await request(app).post('/api/v1/palettes').send(newPalette);
+        const id = response.body.id;
+        console.log('id', id);
+        console.log('response', response.body);
+        const palettes = await database('palettes').select();
+        console.log('DB Palettes', palettes);
+        const palette = await database('palettes').where('id', id).first();
+
+        expect(newPalette.color_1).toEqual(palette.color_1);
+      })
     })
   })
+  
   describe('PUT Methods for Palettes & Projects', () => {
+
   })
   describe('DELETE Methods for Palettes & Projects', () => {
   })
