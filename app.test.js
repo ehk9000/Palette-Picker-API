9ec60a4a749bb9db4524649b07a74f16cd7b8164
RyanDBarnett <conner2468@gmail.com>
2019-07-08 20:01:28 -0600

    Test that Post returns unique name error if project name is taken

--- app.test.js
+++ app.test.js
@@ -7,24 +7,21 @@ const database = require('knex')(configuration)
 
 describe('Server', () => {
   beforeEach(async () => {
-    await database.seed.run()
+    await database.seed.run();
   })
   describe('GET Methods for Palettes & Projects', () => {
     describe('GET /projects && /palettes', () => {
       it('should return all the projects in the DB ', async () => {
-        // set up 
-        const expectedProjects = await database('projects').select()
+        const expectedProjects = await database('projects').select();
         expectedProjects.forEach(project => {
-          project.created_at = project.created_at.toJSON()
-          project.updated_at = project.updated_at.toJSON()
+          project.created_at = project.created_at.toJSON();
+          project.updated_at = project.updated_at.toJSON();
         })
   
-        //  execution
-        const response = await request(app).get('/api/v1/projects')
-        const projects = response.body
+        const response = await request(app).get('/api/v1/projects');
+        const projects = response.body;
     
-        // expectation
-        expect(projects).toEqual(expectedProjects)
+        expect(projects).toEqual(expectedProjects);
       })
 
       it('should return the correct project if there is a name query parameter', async () => {
@@ -39,60 +36,69 @@ describe('Server', () => {
       })
 
       it('should return a 404 error if the name in the query parameter does not match any project names', async () => {
-        const response = await request(app).get('/api/v1/projects?name=noproject')
-        expect(response.status).toBe(404)
+        const response = await request(app).get('/api/v1/projects?name=noproject');
+
+        expect(response.status).toBe(404);
       })
 
       it('should return all the palettes in the DB', async () => {
-        const expectedPalettes = await database('palettes').select()
+        const expectedPalettes = await database('palettes').select();
         expectedPalettes.forEach(palette => {
           palette.created_at = palette.created_at.toJSON();
           palette.updated_at = palette.updated_at.toJSON();
         })
 
-        const response = await request(app).get('/api/v1/palettes')
+        const response = await request(app).get('/api/v1/palettes');
         const palettes = response.body;
+
         expect(palettes).toEqual(expectedPalettes);
       })
     })
     describe('GET /projects/:id && /palettes/:id', () => {
       it('should return a single project with the id in the params', async () => {
-        const expectedProject = await database('projects').first()
-        expectedProject.created_at = expectedProject.created_at.toJSON()
-        expectedProject.updated_at = expectedProject.updated_at.toJSON()
-        
+        const expectedProject = await database('projects').first();
         const id = expectedProject.id;
+        expectedProject.created_at = expectedProject.created_at.toJSON();
+        expectedProject.updated_at = expectedProject.updated_at.toJSON();
   
-        const response = await request(app).get(`/api/v1/projects/${id}`)
+        const response = await request(app).get(`/api/v1/projects/${id}`);
         const project = response.body;
         
-  
-        expect(project).toEqual(expectedProject)
+        expect(project).toEqual(expectedProject);
       })
       it('should return a single palette with the id in the params', async () => {
         const expectedPalette = await database('palettes').first();
+        const id = expectedPalette.id;
         expectedPalette.created_at = expectedPalette.created_at.toJSON();
         expectedPalette.updated_at = expectedPalette.updated_at.toJSON();
 
-        const id = expectedPalette.id;
-
-        const response = await request(app).get(`/api/v1/palettes/${id}`)
+        const response = await request(app).get(`/api/v1/palettes/${id}`);
         const palette = response.body;
 
-        expect(palette).toEqual(expectedPalette)
+        expect(palette).toEqual(expectedPalette);
       })
     })
   })
   describe('POST Methods for Palettes & Projects', () => {
     it('should post a new project to the database', async () => {
-      const newProject = {name: 'BYOB'}
+      const newProject = {name: 'BYOB'};
 
-      const response = await request(app).post('/api/v1/projects/').send(newProject)
+      const response = await request(app).post('/api/v1/projects/').send(newProject);
       const id = response.body.id;
       const project = await database('projects').where('id', id).first();
 
-      expect(newProject.name).toEqual(project.name)
-    })
+      expect(newProject.name).toEqual(project.name);
+    });
+
+    it('should not post a project to the database if the given name is already used', async () => {
+      const newProject = {name: "Test"};
+
+      const response = await request(app).post('/api/v1/projects/').send(newProject);
+
+      expect(response.status).toEqual(422)
+      expect(response.body).toEqual('Project name with Test already exists. Please provide a unique name');
+    });
+
     it('should post a new palette to the database', async () => {
       const newPalette = {
         name: 'new fun palette',
@@ -102,10 +108,10 @@ describe('Server', () => {
         color_4: 'eeeeee',
         color_5: '999999',
         project_name: 'Test'
-      }
+      };
 
       const response = await request(app).post('/api/v1/palettes/').send(newPalette);
-      const id = response.body.id
+      const id = response.body.id;
       const palette = await database('palettes').where('id', id).first();
 
       expect(newPalette.color_1).toEqual(palette.color_1);
@@ -117,15 +123,14 @@ describe('Server', () => {
       const project = await database('projects').first();
       let expectedProject = project;
       expectedProject.name = 'Change Project';
-      expectedProject.created_at = expectedProject.created_at.toJSON()
-      expectedProject.updated_at = expectedProject.updated_at.toJSON()
+      expectedProject.created_at = expectedProject.created_at.toJSON();
+      expectedProject.updated_at = expectedProject.updated_at.toJSON();
 
       const id = project.id;
       const response = await request(app).put(`/api/v1/projects/${id}`).send(expectedProject);
 
-
       expect(id).toEqual(response.body.id);
-      expect(response.body).toEqual(expectedProject)
+      expect(response.body).toEqual(expectedProject);
 
     })
     it('should update palette based on id', async () => {
@@ -134,15 +139,15 @@ describe('Server', () => {
       expectedPalette.name = 'Change palette';
       expectedPalette.color_1 = 'A3HTB';
       expectedPalette.color_3 = '1298B';
-      expectedPalette.created_at = expectedPalette.created_at.toJSON()
-      expectedPalette.updated_at = expectedPalette.updated_at.toJSON()
+      expectedPalette.created_at = expectedPalette.created_at.toJSON();
+      expectedPalette.updated_at = expectedPalette.updated_at.toJSON();
       
 
       const id = palette.id;
       const response = await request(app).put(`/api/v1/palettes/${id}`).send(expectedPalette);
       
       expect(id).toEqual(response.body.id);
-      expect(response.body).toEqual(expectedPalette)
+      expect(response.body).toEqual(expectedPalette);
     })
   })
   describe('DELETE Methods for Palettes & Projects', () => {
@@ -153,7 +158,7 @@ describe('Server', () => {
       await request(app).delete(`/api/v1/projects/${id}`);
 
       const deletedProject = await database('projects').where({id: id}).first();
-      const deletedPalettes = await database('palettes').where({project_id: id}).select()
+      const deletedPalettes = await database('palettes').where({project_id: id}).select();
 
       expect(deletedProject).toEqual(undefined);
       expect(deletedPalettes.length).toEqual(0);
